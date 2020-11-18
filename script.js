c2b641a415c74512d22474dda126a3dc726f256c
Christian <christian.quinlan36@gmail.com>
2020-11-18 21:02:30 +0000

    added condition to check if data.quoteAuthor is empty. if so then it will add Unknown author to the page

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+
 //Get quote from API
 async function getQuote(){
   const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
@@ -6,7 +12,15 @@ async function getQuote(){
   try {
     const response = await fetch (proxyUrl + apiUrl);
     const data = await response.json();
-    console.log(data);
+
+    // check if author text is empty, if so author.text will read "unknown"
+    if(data.quoteAuthor === ""){
+      authorText.innerText = "Unknown";
+    }else{
+      authorText.innerText = data.quoteAuthor;
+    }
+
+    
   } catch (error) {
     getQuote();
     console.log("whoops, its not working", error);
