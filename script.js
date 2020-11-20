395195aa4792b6a1a373c927b8d4fe9353b0cec5
Christian <christian.quinlan36@gmail.com>
2020-11-20 21:23:58 +0000

    minor fixes to rendering quotes

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
 
 let apiQuotes = [];
 
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("quote-author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+
 //Show new quote
 function newQuote(){
   //Pick a random quote for apiQutes array
   const quote = apiQuotes[Math.floor(Math.random()* apiQuotes.length)];
   console.log(quote);
+
+// check if author text is empty, if so author.text will read "unknown"
+if(quote.author === ""){
+  authorText.innerText = "Unknown";
+}else{
+  authorText.innerText = quote.author;
 }
 
-const quoteContainer = document.getElementById("quote-container");
-const quoteText = document.getElementById("quote");
-const authorText = document.getElementById("author");
-const twitterBtn = document.getElementById("twitter");
-const newQuoteBtn = document.getElementById("new-quote");
+//Reduce font-size of quoteText if num of characters are greater than 120
+if(quote.text.length > 120){
+  quoteText.classList.add("long-quote");
+}else if(quote.text.length < 120){
+  quoteText.classList.remove("long-quote");
+}
+quoteText.innerText = quote.text;
+
+}
 
 
 //Get quote from API
@@ -25,12 +41,7 @@ async function getQuotes(){
     apiQuotes = await response.json();
     newQuote();
 
-    // check if author text is empty, if so author.text will read "unknown"
-    if(quote.author === ""){
-      authorText.innerText = "Unknown";
-    }else{
-      authorText.innerText = quote.author;
-    }
+    
     
   } catch (error) {
     //Catch error here
