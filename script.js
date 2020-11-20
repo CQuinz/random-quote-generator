5526061b37ebd4fb9102338a3d5618903550c176
Christian <christian.quinlan36@gmail.com>
2020-11-20 20:42:53 +0000

    merged new working server into main branch

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
-//Get quote from API
-async function getQuote(){
-  const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-  const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+let apiQuotes = [];
 
-  try {
-    const response = await fetch (proxyUrl + apiUrl);
-    const data = await response.json();
-    console.log(data);
-  } catch (error) {
-    getQuote();
-    console.log("whoops, its not working", error);
-  }
-
+//Show new quote
+function newQuote(){
+  //Pick a random quote for apiQutes array
+  const quote = apiQuotes[Math.floor(Math.random()* apiQuotes.length)];
+  console.log(quote);
 }
 
+//Get quote from API
+async function getQuotes(){
+  
+  const apiUrl = 'https://type.fit/api/quotes';
+  
+  try {
+    const response = await fetch (apiUrl);
+    apiQuotes = await response.json();
+    // console.log(apiQuotes[89]);
+    newQuote();
+    
+  } catch (error) {
+    //Catch error here
+    getQuotes();
+    console.log("whoops, its not working", error);
+  }
+}
+
+  
+
+
+
 //Onload
-getQuote();
+getQuotes();
